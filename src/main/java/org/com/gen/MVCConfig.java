7045dddfb64422822049d327d912529a75d0991a
kavya <ckavya498@gmail.com>
2022-01-16 13:39:24 +0530

    spring

---
+++ src/main/java/org/com/gen/MVCConfig.java
@@ -0,0 +1,13 @@
+package org.com.gen;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.org.gen")
+public class MVCConfig extends WebMvcConfigurerAdapter{
+	
+}
